c49982239f9085ba7700f1507dd55b515b2792c8
marwandhiaurrahman <marwandhiaurrahman@gmail.com>
2022-03-26 09:01:10 +0700

    init

---
+++ app.js
@@ -0,0 +1,152 @@
+const { Client } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+// const { status } = require('express/lib/response');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+// app.use(fileUpload({
+//   debug: true
+// }));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {
+    root: __dirname
+  });
+});
+
+const SESSION_FILE_PATH = './wa-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  puppeteer: {
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+    headless: true,
+  },
+  session: sessionCfg
+});
+
+client.initialize();
+
+// client.on('authenticated', (session) => {
+//   console.log('AUTHENTICATED', session);
+//   sessionCfg = session;
+//   fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//   });
+// });
+
+// Socket.io
+io.on('connection', function (socket) {
+  socket.emit('message', 'Connecting...');
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+  client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+    console.log('Client is ready!');
+  });
+
+  client.on('authenticated', (session) => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  });
+
+  client.on('auth_failure', function (session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+    client.destroy();
+    client.initialize();
+  });
+});
+
+
+const checkRegisteredNumber = async function (number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+client.on('message', message => {
+  console.log(message);
+  if (message.body == 'ping') {
+    message.reply('pong');
+  }
+  else {
+    message.reply(message.body);
+  }
+});
+
+app.post('/send-message', [
+  body('number').notEmpty(),
+  body('message').notEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req).formatWith(({ msg }) => msg);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.mapped() });
+  }
+
+  const number = phoneNumberFormatter(req.body.number);
+  const message = req.body.message;
+  const isRegisteredNumber = await checkRegisteredNumber(number);
+
+  if (!isRegisteredNumber) {
+    return res.status(422).json({ errors: { number: 'Number is not registered' } });
+  }
+
+  client.sendMessage(number, message).then(
+    response => {
+      res.send(response);
+    }
+  ).catch(
+    error => {
+      res.send(error);
+    }
+  );
+});
+
+server.listen(8000, function () {
+  console.log('listening on 8000');
+});
